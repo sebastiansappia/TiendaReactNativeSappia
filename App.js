daf18ca3f594cbf07c5ff2109ff8964333af0129
Sebastian Sappia <sebastian@depilife.com>
2023-06-26 22:42:10 -0300

    act

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './src/screens/MainScreen';
 
+/*
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
+      <MainScreen/>
     </View>
+  );
+}*/
+
+export default function App() {
+  return (
+      <MainScreen/>
   );
 }
 
---
+++ src/screens/MainScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MainScreen = () => {
+    return (
+        <View>
+            <Text>Hola, Coder!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default MainScreen;
